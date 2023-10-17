653072f5af1da59e565a8e6e1c280ef215b69909
Felipeln <76855578+felipeln@users.noreply.github.com>
2023-10-17 18:42:52 -0300

    finish javaScript calculator

---
+++ index.js
@@ -0,0 +1,17 @@
+function salaryCalculator(salaryValue,benefitsValue){
+  let taxValue
+  if (salaryValue >= 0 && salaryValue <= 1100){
+    taxValue = 0.05* salaryValue;
+  }
+  if (salaryValue >= 1000.01 && salaryValue <= 2500.00){
+    taxValue = 0.1* salaryValue;
+  }
+  if (salaryValue > 2500){
+    taxValue = 0.15 * salaryValue;
+  }
+  output = (salaryValue - taxValue) + benefitsValue;
+  console.log(output)
+}
+
+
+salaryCalculator(2000, 250)
